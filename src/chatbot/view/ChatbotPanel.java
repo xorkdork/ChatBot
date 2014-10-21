3d29a4c0c7c667bff36e5a1791b5e22bac0f8f9e
xorkdork <dillmoss@gmail.com>
2014-10-21 12:59:29 -0600

    Gui go

--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,15 +1,49 @@
 package chatbot.view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import chatbot.controller.ChatbotAppController;
 
 public class ChatbotPanel extends JPanel
 {
 	private ChatbotAppController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	public ChatbotPanel(ChatbotAppController baseController) 
 	{
 		this.baseController = baseController;
+		firstButton = new JButton("click pls");
+		firstTextField = new JTextField(25);
+		baseLayout = new SpringLayout();
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
-
+	private void setupPanel()
+	{
+		this.setBackground(Color.LIGHT_GRAY);
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
+	}
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 6, SpringLayout.SOUTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 85, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 145, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 155, SpringLayout.WEST, this);
+	}
+	private void setupListeners()
+	{
+		
+	}
 }
