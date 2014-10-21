2587dc30455889a70f1ec84f7105fdd912568ad0
xorkdork <dillmoss@gmail.com>
2014-10-21 12:34:27 -0600

    Gui start

--- src/chatbot/controller/ChatbotAppController.java
+++ src/chatbot/controller/ChatbotAppController.java
@@ -3,6 +3,7 @@ package chatbot.controller;
 import javax.swing.JOptionPane;
 
 import chatbot.model.Chatbot;
+import chatbot.view.ChatbotFrame;
 import chatbot.view.ChatbotView;
 /**
  * Runs the Chatbot.
@@ -15,9 +16,11 @@ public class ChatbotAppController
 	private Chatbot mySillyChatbot;
 	private String startMessage;
 	private String quitMessage;
+	private ChatbotFrame appFrame;
 	public ChatbotAppController()
 	{
 		applicationView = new ChatbotView(this);
+		appFrame = new ChatbotFrame(this);
 		mySillyChatbot = new Chatbot("Derf");
 		startMessage = "Welcome to the " + mySillyChatbot.getName() + "chatbot. What is your name?";
 		quitMessage = "goodbye cruel user :(";
@@ -32,12 +35,12 @@ public class ChatbotAppController
 	public void start()
 	{
 		String result = applicationView.showChatbotDialog(startMessage);
-		while(!mySillyChatbot.quitChecker(result))
-		{
-			result = mySillyChatbot.processText(result);
-			result = applicationView.showChatbotDialog(result);
-		}
-			quit();
+//		while(!mySillyChatbot.quitChecker(result))
+//		{
+//			result = mySillyChatbot.processText(result);
+//			result = applicationView.showChatbotDialog(result);
+//		}
+//			quit();
 		
 	}
 
