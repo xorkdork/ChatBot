a40b64fa125b29898da05465df799ed9ba7abee1
xorkdork <dillmoss@gmail.com>
2014-10-23 13:35:17 -0600

    gui pretty

--- src/chatbot/view/ChatbotFrame.java
+++ src/chatbot/view/ChatbotFrame.java
@@ -3,12 +3,19 @@ package chatbot.view;
 import javax.swing.JFrame;
 
 import chatbot.controller.ChatbotAppController;
+import java.awt.Color;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class ChatbotFrame extends JFrame
 {
 	private ChatbotPanel basePanel;
 	public ChatbotFrame(ChatbotAppController baseController) 
 	{
+		setTitle("da chatbot");
+		setFont(new Font("Comic Sans MS", Font.ITALIC, 12));
+		setBackground(SystemColor.desktop);
+		setForeground(SystemColor.desktop);
 		basePanel = new ChatbotPanel(baseController);
 		setupFrame();
 	}
@@ -16,6 +23,7 @@ public class ChatbotFrame extends JFrame
 	{
 		this.setContentPane(basePanel);
 		this.setSize(400, 400);
+		this.setResizable(false);
 		setVisible(true);
 	}
 
--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,13 +1,20 @@
 package chatbot.view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 import chatbot.controller.ChatbotAppController;
+import java.awt.SystemColor;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class ChatbotPanel extends JPanel
 {
@@ -15,12 +22,16 @@ public class ChatbotPanel extends JPanel
 	private JButton firstButton;
 	private JTextField firstTextField;
 	private SpringLayout baseLayout;
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
 	public ChatbotPanel(ChatbotAppController baseController) 
 	{
 		this.baseController = baseController;
 		firstButton = new JButton("click pls");
+		firstButton.setFont(new Font("Comic Sans MS", Font.ITALIC, 11));
 		firstTextField = new JTextField(25);
 		baseLayout = new SpringLayout();
+		chatPane = new JScrollPane();
 		
 		
 		
@@ -30,20 +41,37 @@ public class ChatbotPanel extends JPanel
 	}
 	private void setupPanel()
 	{
-		this.setBackground(Color.LIGHT_GRAY);
+		this.setBackground(SystemColor.windowBorder);
 		this.setLayout(baseLayout);
 		this.add(firstButton);
 		this.add(firstTextField);
+		this.add(chatPane);
+		chatArea = new JTextArea(5, 20);
+		add(chatArea);
+		
+		chatArea.setLineWrap(true);
+	
 	}
 	private void setupLayout()
 	{
-		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 6, SpringLayout.SOUTH, firstButton);
-		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 85, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 145, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 155, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatArea, 107, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, chatArea, -5, SpringLayout.NORTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 6, SpringLayout.SOUTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 89, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstTextField, 26, SpringLayout.SOUTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, 271, SpringLayout.WEST, this);
 	}
 	private void setupListeners()
 	{
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				firstTextField.setText(firstTextField.getText()+ ";)");
+			}
+		});
 		
 	}
 }
