17cdb951eb54a829fd9e666ce625073596b67c57
xorkdork <dillmoss@gmail.com>
2014-11-04 13:12:52 -0700

    user class

--- src/chatbot/model/ChatbotUser.java
+++ src/chatbot/model/ChatbotUser.java
@@ -2,5 +2,23 @@ package chatbot.model;
 
 public class ChatbotUser
 {
-
+	private String userName;
+	private int age;
+	private boolean hasTattoos;
+	private boolean needsCorrectiveLenses;
+	public ChatbotUser()
+	{
+		this.userName = "";
+		this.age = -999;
+		this.hasTattoos = false;
+		this.needsCorrectiveLenses = false;
+	}
+	public String getUserName()
+	{
+		return userName;
+	}
+	public int getAge()
+	{
+		return age;
+	}
 }
